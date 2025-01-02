ed94dff3fd759b1d51919c165946d1e1b96388e8
ARNAVA KUMAR SINHA <87260059+ARNAVSINHA1@users.noreply.github.com>
2025-01-02 22:52:36 +0530

    Create code.cpp

---
+++ Company 1 : Microsoft/Day 3/7. Bulls And Cows/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    public:
+        string getHint(string secret, string guess) {
+            int A = 0,B = 0;
+            vector<int> count1(10),count2(10);
+            for (int i = 0; i < secret.length(); ++i)
+                if (secret[i] == guess[i])
+                    ++A;
+                else {
+                    ++count1[secret[i] - '0'];
+                    ++count2[guess[i] - '0'];
+                }
+            for (int i = 0; i < 10; ++i)
+                B += min(count1[i], count2[i]);
+            return to_string(A) + "A" + to_string(B) + "B";
+        }
+};
