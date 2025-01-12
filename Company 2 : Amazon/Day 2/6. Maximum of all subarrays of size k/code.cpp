f33c0815c6b35cd828feaf4ca918d06ec299b044
ARNAVA KUMAR SINHA <87260059+ARNAVSINHA1@users.noreply.github.com>
2025-01-12 14:58:09 +0530

    Update code.cpp

--- Company 2 : Amazon/Day 2/6. Maximum of all subarrays of size k/code.cpp
+++ Company 2 : Amazon/Day 2/6. Maximum of all subarrays of size k/code.cpp
@@ -10,25 +10,23 @@ class Solution {
   public:
     // Function to find maximum of each subarray of size k.
     vector<int> maxOfSubarrays(vector<int>& arr, int k) {
-        // code here
-        priority_queue<pair<int, int>> pq; 
-        vector<int> result;
-        int n = arr.size(); 
+        // your code here
+        vector<int> res;
 
-        for (int i = 0; i < k; i++) {
-            pq.push({arr[i], i});
+   int n = arr.size();
+    for (int i = 0; i <= n - k; i++) {
+      
+        // Find maximum of subarray beginning
+        // with arr[i]
+        int max = arr[i];
+        for (int j = 1; j < k; j++) {
+            if (arr[i + j] > max)
+                max = arr[i + j];
         }
-        result.push_back(pq.top().first);
-
-        for (int i = k; i < n; i++) {
-            pq.push({arr[i], i});
-            while (pq.top().second <= i - k) {
-                pq.pop();
-            }
-            result.push_back(pq.top().first);
-        }
-
-        return result;
+        res.push_back(max);
+    }
+  
+    return res;
     }
 };
 
